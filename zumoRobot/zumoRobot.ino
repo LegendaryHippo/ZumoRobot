dbca25df1ffd85b2cd0fad27c0b07d952268740d
rrishaug <rrishaugr@gmail.com>
2015-11-02 13:23:11 +0100

    Motor test

--- zumoRobot/zumoRobot.ino
+++ zumoRobot/zumoRobot.ino
@@ -4,8 +4,6 @@
 #include <QTRSensors.h>
 #include <ZumoReflectanceSensorArray.h>
 
-// HYMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-
 const int M_RIGHT_CONTROL = 7;
 const int M_LEFT_CONTROL = 8;
 const int M_RIGHT_PWM = 9;
@@ -13,16 +11,19 @@ const int M_LEFT_PWM = 10;
 //pin
 const int LED_PIN = 13;
 
+const int MAX_SPEED = 400;
+
 ZumoMotors motors;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_PIN, OUTPUT);
-
-  motors.setSpeeds(200, -200);
+  
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(LED_PIN, HIGH);
+  motors.setSpeeds(200, -200);
 }
