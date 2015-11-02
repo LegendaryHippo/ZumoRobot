3e321041e13364c5e48a8483dd20d614344381fc
Adaptor-Face <kristoffer_rogne@hotmail.com>
2015-11-02 13:50:54 +0100

    Testing branch

--- zumoRobot/zumoRobot.ino
+++ zumoRobot/zumoRobot.ino
@@ -5,17 +5,12 @@
 #include <ZumoReflectanceSensorArray.h>
 
 // HYMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-
+//kristoffer R
 const int M_RIGHT_CONTROL = 7;
 const int M_LEFT_CONTROL = 8;
 const int M_RIGHT_PWM = 9;
 const int M_LEFT_PWM = 10;
-<<<<<<< HEAD
-//pin
 const int LED_PIN = 13;
-
-=======
->>>>>>> a5acef8c32ea5c267006f465b418d16f75ee4eff
 
 void setup() {
   // put your setup code here, to run once:
