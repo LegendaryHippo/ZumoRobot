5ebcfc286542120d072719ff9382eba9ddafad73
erikhny <erikhny@gmail.com>
2015-11-02 12:44:09 +0100

    sut pik
    xddddddddddddddd

--- zumoRobot/zumoRobot.ino
+++ zumoRobot/zumoRobot.ino
@@ -3,7 +3,7 @@ const int M_LEFT_CONTROL = 8;
 const int M_RIGHT_PWM = 9;
 const int M_LEFT_PWM = 10;
 //æsj
-
+//vibrator-face = baddie
 void setup() {
   // put your setup code here, to run once:
    int testtesttest;
