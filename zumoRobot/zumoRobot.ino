148dbea23a0c5b19094d74cfb9cdffe2bd9d8928
rrishaug <rrishaugr@gmail.com>
2015-11-02 14:25:27 +0100

    kalibrering av refSensor

--- zumoRobot/zumoRobot.ino
+++ zumoRobot/zumoRobot.ino
@@ -16,16 +16,41 @@ ZumoReflectanceSensorArray refSensors;
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_PIN, OUTPUT);
+  //motors.setSpeeds(-400, 400);
   refSensors.init();
   Serial.begin(9600);
+  calibrateRefSensor();
+
+
+  
 
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  unsigned int sensors[6];
+  int position = refSensors.readLine(sensors);
+  Serial.println(position);
 
 }
+
+void calibrateRefSensor() {
+  for (int i = 0; i < 80; i++)
+  {
+    if ((i > 10 && i <= 30) || (i > 50 && i <= 70))
+      motors.setSpeeds(-200, 200);
+    else
+      motors.setSpeeds(200, -200);
+    refSensors.calibrate();
+
+    // Since our counter runs to 80, the total delay will be
+    // 80*20 = 1600 ms.
+    delay(20);
+  }
+  motors.setSpeeds(0, 0);
+}
+
 
 
 void motorTest() {
