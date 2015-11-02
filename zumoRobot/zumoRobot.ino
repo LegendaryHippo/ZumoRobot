699efe9bec8728bbaa3f986370112988ee8584a3
rrishaug <rrishaugr@gmail.com>
2015-11-02 13:15:57 +0100

    stuff

--- zumoRobot/zumoRobot.ino
+++ zumoRobot/zumoRobot.ino
@@ -10,16 +10,16 @@ const int M_RIGHT_CONTROL = 7;
 const int M_LEFT_CONTROL = 8;
 const int M_RIGHT_PWM = 9;
 const int M_LEFT_PWM = 10;
-<<<<<<< HEAD
 //pin
 const int LED_PIN = 13;
 
-=======
->>>>>>> a5acef8c32ea5c267006f465b418d16f75ee4eff
+ZumoMotors motors;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_PIN, OUTPUT);
+
+  motors.setSpeeds(200, -200);
 }
 
 void loop() {
